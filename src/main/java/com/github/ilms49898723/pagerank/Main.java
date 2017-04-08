adfd3a0fe37cb991a6c734eb1fcc9ef5726d5fb7
littlebird <ilms49898723@gmail.com>
2017-04-08 20:03:05 +0900

    update

--- src/main/java/com/github/ilms49898723/pagerank/Main.java
+++ src/main/java/com/github/ilms49898723/pagerank/Main.java
@@ -2,6 +2,6 @@ package com.github.ilms49898723.pagerank;
 
 public class Main {
     public static void main(String[] args) {
-        new PageRank().start(args);
+        PageRank.start(args);
     }
 }
